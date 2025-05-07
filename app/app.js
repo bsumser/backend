7e0acfa85904787e7d4dc3e175901f6748fed2d0
Brett Sumser <bsumser@gmail.com>
2025-05-07 00:51:08 -0700

    fix port conflict

--- app/app.js
+++ app/app.js
@@ -11,11 +11,3 @@ app.use(routes);
 
 // Export the app for testing purposes
 export { app };
-
-// Exporting the listen function for starting the server
-if (process.env.NODE_ENV !== 'test') {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
--- app/server.js
+++ app/server.js
@@ -2,9 +2,12 @@
 import { app } from './app.js';
 
 const PORT = process.env.PORT || 8081;
+let server;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+// Exporting the listen function for starting the server
+if (process.env.NODE_ENV !== 'test') {
+  server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 export { server };
