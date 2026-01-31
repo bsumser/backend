84579d9dc84365de4731048d0b77f1aa178547ba
Brett Sumser <bsumser@gmail.com>
2026-01-31 10:15:51 -0800

    change port

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 
 	srv := api.CreateServer()
 
-	fmt.Println("Server running on port :5000")
-	http.ListenAndServe(":5000", srv.Router)
+	fmt.Println("Server running on port :8080")
+	http.ListenAndServe(":8080", srv.Router)
 }
